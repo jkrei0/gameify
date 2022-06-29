6df79c1ed18ce0b37d9c9752085128b791f07309
jkrei0 <jkrei021@gmail.com>
2022-06-28 22:20:40 -0400

    Added update, draw & followSprite functions

--- gameify/collision.js
+++ gameify/collision.js
@@ -2,7 +2,7 @@ import { vectors } from "./vector.js"
 
 "use strict"
 
-/** Shapes and collision detection
+/** Shapes and collision detection for use in gameify
  * @global
  */
 export let shapes = {
@@ -12,8 +12,12 @@ export let shapes = {
      * @param {String} type - The shape type
      */
     Shape: function (type) {
-        /** The type  */
+        /** A string represeting the type of shape */
         this.type = type || "Shape";
+        
+        /** The position of the shape, often locked to the position of a sprite that it represents
+         * @type {gameify.Vector2d}
+         */
         this.position = vectors.vectors.ZERO();
 
         /** Check if this shape collides with another shape
@@ -21,7 +25,36 @@ export let shapes = {
          * @param {shapes.Shape} obj - 
          */
         this.collidesWith = (obj) => {
-            throw new Error('CollidesWith must be implemented by each specific shape type and is not available in the Shape class');
+            throw new Error("shape.collidesWith must be implemented by each specific shape type");
+        }
+
+        /** The sprite this shape is following
+         * @private
+         */
+        this.followedSprite = undefined;
+        
+        /** Make the shape follow the position of a sprite. This should be done automatically if you attach it via the sprite
+         * @param {gameify.Sprite} sprite - The sprite to follow
+         * @package
+         */
+        this.followSprite = (sprite) => {
+            this.followedSprite = sprite;
+        }
+
+        /** Update the shape. This should be done automatically if you attach it via the sprite
+         * @package
+         */
+        this.update = () => {
+            this.position.x = this.followedSprite.position.x;
+            this.position.y = this.followedSprite.position.y;
+        }
+
+        /** Draw a hitbox for debugging
+         * @virtual
+         * @param {CanvasRenderingContext2D} context - The rendering context to draw to
+         */
+        this.draw = (canvas) => {
+            throw new Error("shape.draw must be implemented by each specific shape type");
         }
     },
     /** A circle shape
@@ -31,12 +64,39 @@ export let shapes = {
     Circle: function (x, y, radius) {
         shapes.Shape.call(this, "Circle");
 
+        /** The center of the circle, relative to the circle's position
+         * @type {gameify.Vector2d}
+         */
         this.centerOffset = new vectors.Vector2d(x, y);
 
+        /** The radius of the circle */
         this.radius = radius;
 
-        this.collidesWith = () => {
+        this.collidesWith = (obj, recursion) => {
+            if (obj.type === "Circle") {
+                let pos1 = this.position.add( this.centerOffset );
+                let pos2 = obj.position.add( obj.position.centerOffset );
+
+                return ( pos1.subtract(pos2).getDistance() < this.radius + obj.radius );
+
+            } else {
+                // don't create an infinite recursion loop if neither object has a collision function
+                if (recursion) {
+                    throw new Error("Collision between these shapes is not supported.");
+                }
+                // else see if the passed object can handle the request
+                return obj.collidesWith(this, true);
+            }
+        }
+
+        this.draw = (context) => {
+            context.beginPath();
+            context.arc( this.position.x + this.centerOffset.x,
+                         this.position.y + this.centerOffset.y,
+                         this.radius, 0, 2 * Math.PI );
+            context.stroke();
 
         }
+
     }
 };
