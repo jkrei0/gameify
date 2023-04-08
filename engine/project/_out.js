e40b5ccac96492229df0c3c345e4c304ae43f4af
jkrei0 <jkrei021@gmail.com>
2023-04-08 16:45:20 -0400

    $get Type:: (without a name) returns all objects of that type

--- engine/project/_out.js
+++ engine/project/_out.js
@@ -21,9 +21,21 @@ const deserializeObject = () => {
  * @param {string} name - The name of the object to access
  */
 export const $get = (name) => {
+
+    const type = name.split('::')[0];
+    const oName = name.split('::')[1];
+    if (oName === "" && window.__s_objects[type]) {
+        const names = [];
+        const results = [];
+        for (const obj in window.__s_objects[type]) {
+            names.push(name + obj);
+            results.push($get(name + obj));
+        }
+
+        return results;
+    }
+
     if (!__objects[name]) {
-        const type = name.split('::')[0];
-        const oName = name.split('::')[1];
         if (window.__s_objects[type] && window.__s_objects[type][oName]) {
             if (window.__s_objects[type][oName] === false) {
                 console.warn(`Cannot deserialize ${type}::${oName}`);
@@ -65,8 +77,11 @@ console.info('Engine loaded');
 
 setTimeout(() => {
     // Start the game
+    if (!$get('Screen::')[0]) {
+        console.error('Your game has no Screen object!');
+    }
+    $get('Screen::')[0].startGame();
     console.info('Game started');
-    $get('Screen::Screen').startGame();
 }, 200);
 
 export default $get;
