f85f4236a84567b3e2d0be76bece0e3cc331d845
jkrei0 <jkrei021@gmail.com>
2023-06-11 16:05:44 -0400

    Shapes actually do something now - Removed overcomplicated rotation functionality - Shapes have fill/stroke color and debug draw() function - Added Rectangle shape

--- gameify/collision.js
+++ gameify/collision.js
@@ -20,41 +20,15 @@ export let shapes = {
      * @param {string} type - The shape type
      */
     Shape: function (type) {
-        /** A string represeting the type of shape */
+        /** A string represeting the type of shape, eg "Circle"
+         * @type {String}
+        */
         this.type = type || "Shape";
         
         /** The position of the shape, often locked to the position of a sprite that it represents
          * @type {gameify.Vector2d}
          */
         this.position = vectors.vectors.ZERO();
-
-        /** The point that the shape rotates around, relative to its position
-         * @type {gameify.Vector2d}
-         */
-        this.rotationPoint = vectors.vectors.ZERO();
-
-        /** The rotation of the shape, in degrees, rotated around the shape's rotation point */
-        this.rotation = 0;
-
-        /** Rotate the shape around its rotationPoint
-         * @param {number} rotation - The (absolute) rotation in degrees
-         *//**
-         * Rotate the shape around a given point (also sets the rotationPoint)
-         * @param {number} rotation - The (absolute) rotation in degrees
-         * @param {number} x - The x position, relative to the shape's position, to rotate around
-         * @param {number} y - The y position, relative to the shape's position, to rotate around
-         *//**
-         * Rotate the shape around a given point (also sets the rotationPoint)
-         * @param {number} rotation - The (absolute) rotation in degrees
-         * @param {gameify.Vector2d} point - The point (relative to the shape's position) to rotate around
-         */
-        this.setRotation = (rotation, x, y) => {
-            let rotationVector = this.rotationPoint;
-            if (x !== undefined && y === undefined) {
-                // 3rd overload, x is a vector
-            }
-            // TODO shape rotation
-        }
 
         /** Check if this shape collides with another shape
          * @virtual
@@ -63,6 +37,15 @@ export let shapes = {
         this.collidesWith = (obj) => {
             throw new Error("shape.collidesWith must be implemented by each specific shape type");
         }
+
+        /** The stroke color when this shape is drawn
+         * @type {String}
+        */
+        this.strokeColor = "#25ac",
+        /** The fill color when this shape is drawn
+         * @type {String}
+         */
+        this.fillColor = "#58c3",
 
         /** Draw a hitbox for debugging
          * @virtual
@@ -83,20 +66,23 @@ export let shapes = {
     Circle: function (x, y, radius) {
         shapes.Shape.call(this, "Circle");
 
-        /** The center of the circle, relative to the circle's position
-         * @type {gameify.Vector2d}
-         */
-        this.centerOffset = new vectors.Vector2d(x, y);
+        if (typeof x !== "number" || typeof y !== "number" || typeof radius !== "number") {
+            throw "Circle x, y, and radius must be numbers";
+        }
 
-        /** The radius of the circle */
+        this.position = new vectors.Vector2d(x, y);
+
+        /** The radius of the circle
+         * @type {Number}
+        */
         this.radius = radius;
-
+        
+        /** Check if this shape collides with another shape
+         * @param {shapes.Shape} obj - The object to check for collision
+         */
         this.collidesWith = (obj, recursion) => {
             if (obj.type === "Circle") {
-                let pos1 = this.position.add( this.centerOffset );
-                let pos2 = obj.position.add( obj.position.centerOffset );
-
-                return ( pos1.subtract(pos2).getDistance() < this.radius + obj.radius );
+                return ( this.position.subtract(obj.position).getDistance() < this.radius + obj.radius );
 
             } else {
                 // don't create an infinite recursion loop if neither object has a collision function
@@ -104,17 +90,110 @@ export let shapes = {
                     throw new Error("Collision between these shapes is not supported.");
                 }
                 // else see if the passed object can handle the request
-                return obj.collidesWith(this, true);
+                return obj.collidesWith(this, /*recursion=*/true);
             }
         }
 
+        /** Draw the shape to a given context
+         * @param {CanvasRenderingContext2D} context - The canvas context to draw to
+         */
         this.draw = (context) => {
+            context.strokeStyle = this.strokeColor;
+            context.fillStyle = this.fillColor;
             context.beginPath();
-            context.arc( this.position.x + this.centerOffset.x,
-                         this.position.y + this.centerOffset.y,
+            context.arc( this.position.x,
+                         this.position.y,
                          this.radius, 0, 2 * Math.PI );
             context.stroke();
+            context.fill();
         }
 
+    },
+    /** A rectangle shape
+     * @constructor
+     * @alias gameify.shapes.Rectangle
+     * @extends gameify.shapes.Shape
+     * @param {number} x - The x position
+     * @param {number} y - The y position
+     * @param {number} width - The rectangle width
+     * @param {number} height - The rectangle height
+     */
+    Rectangle: function (x, y, width, height) {
+        shapes.Shape.call(this, "Rectangle");
+
+        if (typeof x !== "number"
+            || typeof y !== "number"
+            || typeof width !== "number"
+            || typeof height !== "number"
+        ) {
+            throw "Circle x, y, and radius must be numbers";
+        }
+
+        if (width < 0 || height < 0) {
+            width = Math.abs(width);
+            height = Math.abs(height);
+            console.warn('Rectangle width and height should be >= 0. Using absolute values instead.');
+        }
+        
+        this.position = new vectors.Vector2d(x, y);
+        /** The size (width and height) of the rectangle
+         * @type {gameify.Vector2d}
+         */
+        this.size = new vectors.Vector2d(width, height);
+
+        /** Check if this shape collides with another shape
+         * @param {shapes.Shape} obj - The object to check for collision
+         */
+        this.collidesWith = (obj, recursion) => {
+            if (obj.type === "Rectangle") {
+                if (this.x < obj.x + obj.size.x && this.x + this.size.x > obj.x
+                    && this.y < obj.y + obj.size.y && this.y + this.size.y > obj.y
+                ) {
+                    return true;
+
+                } else {
+                    return false;
+                }
+
+            } else if (obj.type === "Circle") {
+                const topRight = new vectors.Vector2d(this.position);
+                topRight.x += this.size.x;
+                const bottomLeft = new vectors.Vector2d(this.position);
+                bottomLeft.y += this.size.y;
+                const bottomRight = new vectors.Vector2d(this.position).add(this.size);
+
+                if (obj.position.distanceTo(this.position, topRight) < obj.radius
+                    || obj.position.distanceTo(this.position, bottomLeft) < obj.radius
+                    || obj.position.distanceTo(topRight, bottomRight) < obj.radius
+                    || obj.position.distanceTo(bottomLeft, bottomRight) < obj.radius
+                    || (obj.position.x > this.position.x && obj.position.y > this.position.y
+                        && obj.position.x < bottomRight.x && obj.position.y < bottomRight.y)
+                ) {
+                    return true;
+                }
+                return false;
+
+            } else {
+                // don't create an infinite recursion loop if neither object has a collision function
+                if (recursion) {
+                    throw new Error("Collision between these shapes is not supported.");
+                }
+                // else see if the passed object can handle the request
+                return obj.collidesWith(this, /*recursion=*/true);
+            }
+        }
+
+        /** Draw the shape to a given context
+         * @param {CanvasRenderingContext2D} context - The canvas context to draw to
+         */
+        this.draw = (context) => {
+            context.strokeStyle = this.strokeColor;
+            context.fillStyle = this.fillColor;
+            context.beginPath();
+            context.rect(this.position.x, this.position.y,
+                         this.size.x, this.size.y);
+            context.stroke();
+            context.fill();
+        }
     }
 };
