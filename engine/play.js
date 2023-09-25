b89b02fb976da12861dda4334bc3a6935434ed53
jkrei0 <jkrei021@gmail.com>
2023-09-24 20:55:10 -0400

    Play screen 'editor' button opens current project

--- engine/play.js
+++ engine/play.js
@@ -9,6 +9,9 @@ const gameTitle = window.location.hash.split('/')[1];
 
 if (accountName && gameTitle) {
     document.querySelector('#project-name').innerText = accountName + '/' + gameTitle;
+    document.querySelector('#open-editor-button').onclick = () => {
+        window.location.href = `/engine/engine.html#${accountName}/${gameTitle}`;
+    }
 
 } else {
     document.querySelector('#project-name').innerText = 'No open project';
