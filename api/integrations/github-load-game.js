983bc6d7414fe9b5e255dd02b9caea14f84f3c94
jkrei0 <jkrei021@gmail.com>
2023-12-02 10:58:16 -0500

    add logs

--- api/integrations/github-load-game.js
+++ api/integrations/github-load-game.js
@@ -24,6 +24,8 @@ export default async function handler(request, response) {
     // If gh.token is undefined, you can still clone public repositories
     const cloneUrl = `https://x-access-token:${ghDetails.token}@github.com/` + query.repo;
 
+    console.log(dir, fs.existsSync(dir), process.cwd(), __dirname, cloneUrl);
+
     if (fs.existsSync(dir)) fs.rmSync(dir, { recursive: true, force: true });
     //fs.mkdirSync(dir, { recursive: true });
     git.clone({ fs, http, dir, url: cloneUrl }).then(async () => {
