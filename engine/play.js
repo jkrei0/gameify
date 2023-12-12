d1f919a339267ad29d5ddf5af3533ef8256275dc
jkrei0 <jkrei021@gmail.com>
2023-12-12 17:13:18 -0500

    Re-add play.js functionality that shouldn't've been removed

--- engine/play.js
+++ engine/play.js
@@ -4,3 +4,16 @@ const embedURL = 'https://gameify-embed.vercel.app/embed.html';
 const gameFrame = document.querySelector('#game-frame');
 const gameWindow = gameFrame.contentWindow;
 gameWindow.location.href = embedURL + window.location.hash;
+
+const accountName = window.location.hash.split('/')[0].replace('#', '');
+const gameTitle = window.location.hash.split('/')[1];
+
+if (accountName && gameTitle) {
+    document.querySelector('#project-name').innerText = accountName + '/' + gameTitle;
+    document.querySelector('#open-editor-button').onclick = () => {
+        window.location.href = `/engine/engine.html#${accountName}/${gameTitle}`;
+    }
+
+} else {
+    document.querySelector('#project-name').innerText = 'No open project';
+}
