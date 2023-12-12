83d9ed2cc26a0827f16c6cc99e7cbff8b8bedac6
jkrei0 <jkrei021@gmail.com>
2023-12-12 17:16:34 -0500

    Empty ui elements file

---
+++ gameify/ui.js
@@ -0,0 +1,18 @@
+import { vectors } from "./vector.js"
+
+/** User interfaces elements for use in Gameify
+ * @example // Use ui via gameify
+ * // This is the most common way
+ * import { gameify } from "./gameify/gameify.js"
+ * let myButton = new gameify.ui.Button("Hello, World!", 0, 0);
+ * @example // Import just ui
+ * import { ui } from "./gameify/ui.js"
+ * let myButton = new ui.Button("Hello, World!", 0, 0);
+ * @global
+ */
+export let ui = {
+    /** A clickable button */
+    Button: function (text, x = 0, y = 0) {
+        
+    }
+};
