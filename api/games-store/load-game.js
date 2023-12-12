8f2302d620f16fc86842fb247020fabf628daa06
jkrei0 <jkrei021@gmail.com>
2023-12-12 16:31:42 -0500

    Allow CORS when getting game data + /gameify/* should have CORS enabled anyway

--- api/games-store/load-game.js
+++ api/games-store/load-game.js
@@ -2,14 +2,28 @@
 import { loadGame } from '../../api-util/mongo.js';
 
 export default async function handler(request, response) {
+
+
     let body;
     try {
         body = JSON.parse(request.body);
     } catch {
-        return response.status(400).json({ error: 'invalid request body' });
+        response.writeHead(400, {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        });
+        response.write(JSON.stringify({ error: 'invalid request body' }));
+        response.end();
+
+        return //response.json({ error: 'invalid request body' });
     }
 
     const result = await loadGame(body);
 
-    response.status(200).json(result);
+    response.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+    });
+    response.write(JSON.stringify(result));
+    response.end();
 }
