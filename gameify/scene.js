c86891e8ede754e0725295920f0b19a412bd3823
jkrei0 <jkrei021@gmail.com>
2023-12-20 15:56:09 -0500

    Converted Scene to es6 class, added to/fromJSON methods

--- gameify/scene.js
+++ gameify/scene.js
@@ -33,95 +33,123 @@ export let scenes = {
      * myScreen.startGame();
      * @arg {gameify.Screen} screen - The Screen the Scene should draw to.
      */
-    Scene: function (screen) {
-        if (screen === '_deserialize') {
-            // data - saved data
-            // find - a function to find an object based on a saved name
-            return (data, find) => {
-                const obj = new scenes.Scene();
-                if (data[1]) obj.lock(data[1]);
-                return obj;
-            }
-        }
-        // name - a function to generate a name for an object to be restored later
-        this.serialize = (name) => {
-            return [this.locked];
+    Scene: class {
+        constructor (screen) {
+            this.parent = screen;
         }
 
         /** The user-set update function
          * @private
          */
-        this.updateFunction = null;
+        #updateFunction = null;
+        /** The user-set draw function
+         * @private
+         */
+        #drawFunction = null;
+        /** If the scene is locked */
+        locked = false;
+        /** The parent Screen */
+        parent;
+
+        /** Creates a object from JSON data
+         * @method
+         * @arg {Object|Array} data - Serialized object data (from object.toJSON)
+         * @arg {Function} ref - A function that returns a name for other objects, so they can be restored later
+         * @returns {gameify.Scene}
+        */
+        static fromJSON = (data, find) => {
+            if (Array.isArray(data)) {
+                // Be backwards compatible
+                console.warn('Save is using the old (de)serialization format for Image.');
+                const obj = new scenes.Scene();
+                if (data[1]) obj.lock(data[1]);
+                return obj;
+            }
+
+            const obj = new scenes.Scene(data.parent);
+            if (data.locked) obj.lock(data.locked)
+            return obj;
+        }
+        
+        /** Convert the object to JSON
+         * @method
+         * @arg {string} [key] - Key object is stored under (unused, here for consistency with e.g. Date.toJSON, etc.)
+         * @arg {function} ref - A function that returns a name for other objects, so they can be restored later
+         * @returns {Object}
+         */
+        toJSON = (key, ref) => {
+            return {
+                locked: this.locked,
+                parent: ref(this.parent)
+            };
+        }
 
         /** Set the update function for this scene
+         * @method
          * @param {function} callback - The function that is called every update. Optionally, you can include a delta argument for physics and movement calculations.
          */
-        this.onUpdate = (callback) => {
-            this.updateFunction = callback;
+        onUpdate = (callback) => {
+            this.#updateFunction = callback;
         }
 
         /** Update the scene
-         * @private
+         * @method
          */
-        this.update = (delta) => {
-            if (this.updateFunction == null) {
+        update = (delta) => {
+            if (this.#updateFunction == null) {
                 throw new Error(`You need to specify an update function for this Scene. See ${docs.getDocs("gameify.Scene")} for details`);
             }
             this.parent.keyboard.clearJustPressed();
             this.parent.mouse.clearRecentEvents();
-            this.updateFunction(delta);
+            this.#updateFunction(delta);
         }
 
-        this.locked = false;
         /** Lock the scene, meaning you cannot switch to another scene until it is unlocked with scene.unlock()
+         * @method
          * @param {String} [text] - A helpful message to display when you try to switch scenes
          */
-        this.lock = (text) => {
+        lock = (text) => {
             this.locked = text || "Unlock it to change the scene";
         }
-        /** Unlock the scene, meaning you can now switch to another scene. Scenes are unlocked by default */
-        this.unlock = () => {
+        /** Unlock the scene, meaning you can now switch to another scene. Scenes are unlocked by default
+         * @method
+         */
+        unlock = () => {
             this.locked = false;
         }
 
-        /** The user-set draw function
-         * @private
-         */
-        this.drawFunction = null;
-
         /** Set the draw function for this scene
+         * @method
          * @param {function} callback
          */
-        this.onDraw = (callback) => {
-            this.drawFunction = callback;
+        onDraw = (callback) => {
+            this.#drawFunction = callback;
         }
  
         /** Draw the scene. This is done automatically, you usually shouldn't have to use it yourself.
+         * @method
          */
-        this.draw = () => {
-            if (this.drawFunction == null) {
+        draw = () => {
+            if (this.#drawFunction == null) {
                 throw new Error(`You need to specify a draw function for your Scene. See ${docs.getDocs("gameify.Scene")} for details`);
             }
-            this.drawFunction();
+            this.#drawFunction();
         }
 
-        /** The parent Screen
-         * @private
-         */
-        this.parent = screen;
-
-        /** Run when the scene is set as a Screen's active scene 
+        /** Run when the scene is set as a Screen's active scene
+         * @method
+         * @package
          * @param {gameify.Screen} parent
-         * @private
         */
-        this.onLoad = (parent) => {
+        onLoad = (parent) => {
             this.parent = parent;
         }
 
         /** Run when the scene is set as inactive / replaced by another scene
-         * @private
+         * @method
+         * @package
         */
-        this.onUnload = () => {
+        onUnload = () => {
             this.parent = null;
         }
     },
