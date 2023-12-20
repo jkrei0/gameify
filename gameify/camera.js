109ca10632ba4f98a21c676ec8e245814aa53d39
jkrei0 <jkrei021@gmail.com>
2023-12-20 13:18:52 -0500

    Converted Camera to es6 class

--- gameify/camera.js
+++ gameify/camera.js
@@ -8,7 +8,7 @@ import { vectors }  from "./vector.js"
  * @global
  */
 export let camera = {
-    /** A camera.
+    /** A camera (controls the viewport of the game)
      * @alias gameify.Camera
      * @constructor
      * @example
@@ -18,22 +18,17 @@ export let camera = {
      * myScreen.camera.translate(50, 70);
      * @arg {CanvasRenderingContext2D} context - The canvas context
      */
-    Camera: function (context) {
-        this.context = context;
+    Camera: class {
+        constructor (context) {
+            this.#context = context;
+            this.maxDistance = this.#context.canvas.height * 0.5
+        }
 
         /** The translation speed of the camera. 1 is instant, 0 is stationary.
          * @default 1
          * @type {Number}
          */
-        this.translationSpeed = 1;
-
-        /** Set the translation speed of the camera. 1 is instant, 0 is stationary.
-         * @param {Number} speed - The speed of camera translations
-         */
-        this.setSpeed = (speed) => {
-            this.translationSpeed = speed;
-        }
-
+        translationSpeed = 1;
         /** The maximum distance the camera can be from its target position, in pixels.
          * If the camera goes too far away, it will speed up to stay close to the target.
          * (Note, this distance is measured as a square, not a circle)
@@ -45,26 +40,35 @@ export let camera = {
          * @default canvas height * 0.5
          * @type {Number}
          */
-        this.maxDistance = this.context.canvas.height * 0.5;
-
+        maxDistance;
         /** The minimum distance the camera must be from its target position, in pixels,
          * before the camera starts to move. (Note, this distance is measured as a square, not a circle)
          * @default 1
          * @type {Number}
          */
-        this.minDistance = 1;
+        minDistance = 1;
 
-        // "Private" translation target variable
-        let _translationTarget = new vectors.Vector2d(0, 0);
-        let _currentTranslation = new vectors.Vector2d(0, 0);
+        #context;
+        #translationTarget = new vectors.Vector2d(0, 0);
+        #currentTranslation = new vectors.Vector2d(0, 0);
+
+        /** Set the translation speed of the camera. 1 is instant, 0 is stationary.
+         * @method
+         * @param {Number} speed - The speed of camera translations
+         */
+        setSpeed = (speed) => {
+            this.translationSpeed = speed;
+        }
 
         /** Translate the camera (relative to the current transform)
+         * @method
          * @param {Number} x - The x amount, in pixels, to translate
          * @param {Number} y - The y amount, in pixels, to translate
          *//** Translate the camera (relative to the current transform)
+         * @method
          * @param {gameify.Vector2d} amount - The amount, in pixels, to translate
          */
-        this.translate = (x, y) => {
+        translate = (x, y) => {
             if (x.x != undefined && x.y != undefined) {
                 // Convert vector to x and y
                 y = x.y;
@@ -73,17 +77,19 @@ export let camera = {
             if (typeof x != 'number' || typeof y != 'number') {
                 throw new Error("X and Y position passed to camera.translate are not numbers!");
             }
-            _translationTarget.x += x;
-            _translationTarget.y += y;
+            this.#translationTarget.x += x;
+            this.#translationTarget.y += y;
         }
 
         /** Translate the camera (using absolute positioning)
+         * @method
          * @param {Number} x - The x position to translate to
          * @param {Number} y - The y position to translate to
          *//** Translate the camera (using absolute positioning)
+         * @method
          * @param {gameify.Vector2d} position - The position to translate to
          */
-        this.translateAbsolute = (x, y) => {
+        translateAbsolute = (x, y) => {
             if (x.x != undefined && x.y != undefined) {
                 // Convert vector to x and y
                 y = x.y;
@@ -92,20 +98,22 @@ export let camera = {
             if (typeof x != 'number' || typeof y != 'number') {
                 throw new Error("X and Y position passed to camera.translateAbsolute are not numbers!");
             }
-            _translationTarget.x = x;
-            _translationTarget.y = y;
+            this.#translationTarget.x = x;
+            this.#translationTarget.y = y;
         }
 
         /** Position the center of the screen (using absolute position). Useful for following a player/sprite
+         * @method
          * @param {Number} x - The x position to translate to
          * @param {Number} y - The y position to translate to
          * @param {Number} offsetx - Y offset (from given position)
          * @param {Number} offsety - X offset (from given position)
          *//** Position the center of the screen (using absolute position). Useful for following a player/sprite
+         * @method
          * @param {gameify.Vector2d} position - The position to translate to
          * @param {gameify.Vector2d} [offset=new gameify.Vector2d(0, 0)] - Offset by an amount
          */
-        this.focus = (x, y, ox = 0, oy = 0) => {
+        focus = (x, y, ox = 0, oy = 0) => {
             let position = new vectors.Vector2d(x);
             let offset = new vectors.Vector2d(y);
 
@@ -115,7 +123,7 @@ export let camera = {
                 offset = new vectors.Vector2d(ox, oy);
             }
 
-            const screenSize = new vectors.Vector2d(this.context.canvas.width, this.context.canvas.height);
+            const screenSize = new vectors.Vector2d(this.#context.canvas.width, this.#context.canvas.height);
             let screenOffset = position.subtract(screenSize.multiply(.5)).truncated();
             screenOffset = screenOffset.add(offset);
 
@@ -127,9 +135,10 @@ export let camera = {
         }
 
         /** Reset the camera's (canvas's) transformation
+         * @method
          */
-        this.resetTransform = () => {
-            this.context.setTransform(1, 0, 0, 1, 0, 0);
+        resetTransform = () => {
+            this.#context.setTransform(1, 0, 0, 1, 0, 0);
         }
 
         /** Sets the camera draw mode.
@@ -137,9 +146,10 @@ export let camera = {
          * (draw mode is reset to 'world' each frame).
          * Equivalent to calling camera.resetTransform() or camera.update(0)
          * @tutorial camera
+         * @method
          * @param {String} mode - 'world' or 'ui'
          */
-        this.setDrawMode = (mode) => {
+        setDrawMode = (mode) => {
             mode = mode.toLowerCase();
             if (mode === 'ui') this.resetTransform();
             else if (mode === 'world') this.update(0); // move to position, without actually moving the camera
@@ -148,43 +158,44 @@ export let camera = {
 
         /** Update the camera. If you're using the a gameify.Screen's camera, this will be called automatically.
          * This function rounds translation vectors to whole numbers for better rendering
+         * @method
          * @param {Number} delta - The time, in milliseconds, since the last frame
          */
-        this.update = (delta) => {
+        update = (delta) => {
             this.resetTransform();
 
-            const distX = _translationTarget.x - _currentTranslation.x;
+            const distX = this.#translationTarget.x - this.#currentTranslation.x;
             const distXAbs = Math.abs(distX);
-            const distY = _translationTarget.y - _currentTranslation.y;
+            const distY = this.#translationTarget.y - this.#currentTranslation.y;
             const distYAbs = Math.abs(distY);
 
-            const frameTargetPos = _translationTarget.copy();
+            const frameTargetPos = this.#translationTarget.copy();
 
             // If x or y is w/in min distance, 
-            if (distXAbs < this.minDistance) frameTargetPos.x = _currentTranslation.x;
-            if (distYAbs < this.minDistance) frameTargetPos.y = _currentTranslation.y;
+            if (distXAbs < this.minDistance) frameTargetPos.x = this.#currentTranslation.x;
+            if (distYAbs < this.minDistance) frameTargetPos.y = this.#currentTranslation.y;
 
             if (this.translationSpeed >= 1) {
                 // Move instantly, speed is set to instant
-                _currentTranslation = frameTargetPos.copy();
+                this.#currentTranslation = frameTargetPos.copy();
 
             } else if (distXAbs > this.maxDistance || distYAbs > this.maxDistance) {
                 // Stay inside square boundary w/ side length of 2 * maxDistance
                 if (distXAbs > this.maxDistance) {
-                    _currentTranslation.x = frameTargetPos.x - (Math.sign(distX) * this.maxDistance);
+                    this.#currentTranslation.x = frameTargetPos.x - (Math.sign(distX) * this.maxDistance);
                 }
                 if (distYAbs > this.maxDistance) {
-                    _currentTranslation.y = frameTargetPos.y - (Math.sign(distY) * this.maxDistance);
+                    this.#currentTranslation.y = frameTargetPos.y - (Math.sign(distY) * this.maxDistance);
                 }
 
             } else {
                 // Move by linear interpolation
                 const lerpAmount = 1 - Math.pow(1 - this.translationSpeed, delta);
-                _currentTranslation = _currentTranslation.linearInterpolate(frameTargetPos, lerpAmount);
+                this.#currentTranslation = this.#currentTranslation.linearInterpolate(frameTargetPos, lerpAmount);
             }
 
-            const position = _currentTranslation.truncated();
-            this.context.translate(position.x, position.y);
+            const position = this.#currentTranslation.truncated();
+            this.#context.translate(position.x, position.y);
         }
     }
 }
